37978a952cad9872f06ecb7f9442bb6e7582fc0a
masahiro96848 <feetwcpic9436@gmail.com>
2024-06-02 12:48:22 +0900

    hello worldを出力

---
+++ index.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    hello(name: String!): String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (parent, args) => `Hello ${args.name}`,
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
