97c0985cc532b38db163daa30b0fb52bbde165e5
masahiro96848 <feetwcpic9436@gmail.com>
2024-06-02 17:31:50 +0900

    バックエンドとフロントエンドを連携してgraghQLでデータを呼び出すように実装

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,38 @@
+import { gql, useQuery } from '@apollo/client'
 import './App.css'
 
+const BOOKS = gql`
+  query {
+    test {
+      title
+      author
+    }
+  }
+`
+
+function BookComponent() {
+  const { loading, error, data } = useQuery(BOOKS)
+  console.log(data)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>エラー</p>
+
+  return data.test.map((book, id) => (
+    <div key={id}>
+      <p>
+        {book.author} : {book.title}
+      </p>
+    </div>
+  ))
+}
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <h2>GraphQL Client</h2>
+      <BookComponent />
+    </div>
+  )
 }
 
 export default App
